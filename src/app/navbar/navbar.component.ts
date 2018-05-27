0767ccc19fbc6de2fd73b2ca46b34e701fe9cdbc
Falcon <falcon198198@msn.com>
2018-05-27 13:01:48 -0400

    Added some components for the project

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-auct-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
